5b1c6e21184b0974248287c8743be8f6a3481de9
lgustavocampos <lgustavocampos@gmail.com>
2018-08-26 23:58:10 -0300

    Initial Commit

---
+++ BotService/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BotService
+//
+//  Created by GUSTAVO CAMPOS on 26/08/18.
+//  Copyright © 2018 GUSTAVO CAMPOS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
